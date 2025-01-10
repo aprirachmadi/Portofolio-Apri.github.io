a449c3e9797e1e580c1619c79994bb1a6a8bc48c
Apri Rachmadi <121801142+aprirachmadi@users.noreply.github.com>
2025-01-10 20:11:50 +0700

    fix image path

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import profileImg from "../assets/hero/saya.jpg";
 
 const Hero = () => {
   const scrollToSection = (sectionId: string) => {
@@ -15,7 +16,7 @@ const Hero = () => {
           <div className="w-full md:w-2/3">
             <div className="flex items-center gap-8 mb-6">
               <img
-                src="src/assets/hero/saya.jpg"
+                src={profileImg}
                 alt="Profile"
                 className="w-24 h-24 rounded-full object-cover border-2 border-primary"
               />
